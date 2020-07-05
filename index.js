08c7838e649d1ca7ca615c1b5e473dfc1c0ca788
eXhumer <exhumer1@protonmail.com>
2020-07-05 00:37:48 -0600

    Add boilerpoint notice to scripts

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/*
+ * f1tv-master-link-generator - Script to get authenticated F1TV session media URL from session slug & F1TV credentials
+ * Copyright (C) 2020 eXhumer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const f1tvUtils = require("./f1tvutils");
 const yargs = require("yargs");
 
@@ -43,4 +60,4 @@ yargs
         }
     })
     .help()
-    .argv
+    .argv;
